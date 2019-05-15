891d29e063c63637c2c425434314d3b6f82033d6
danny-nomura <48335432+danny-nomura@users.noreply.github.com>
2019-05-14 21:24:30 -0700

    Most up to date

--- Assignment3_142.cpp
+++ Assignment3_142.cpp
@@ -1,11 +1,15 @@
 //============================================================================
-// Name        : Assignment3_142.cpp
-// Author      : 
-// Version     :
+// Name    	: Assignment3_142.cpp
+// Author  	:
+// Version 	:
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+// A simple memory manager
+// This program takes input from a file with three fields for a process
+// Proccess_ID , ACTION , and PAGE
+// We will simulate FIFO, LRU, and RANDOM swapping policies
 // A simple memory manager
 // This program takes input from a file with three fields for a process
 // Proccess_ID , ACTION , and PAGE
@@ -29,27 +33,37 @@ void LRU(int);
 void RANDOM(int);
 
 // Structure for process objects, with fields for their ID, action, and page num
-typedef struct Process
+typedef struct pageTable
 {
-    int PROCESS_ID;
-    string ACTION;
-    int PAGE;
-}Process;
+	int PROCESS_ID;
+	string ACTION;
+	int PAGE;
+}pageTable;
 
 // Instantiating an array of Process objects to hold them for interpreting
-Process myProcess[MAX_ARRAY_SIZE];
+pageTable myPageTable[MAX_ARRAY_SIZE];
 
 // Instantiating our 20 pages of physical memory
-Process physicalMemory[20];
+pageTable physicalMemory[20];
+
+// Intstantiate an array for swapping
+pageTable swapping[20];
 
 // Main Function
 int main(){
 
-    string fileName = "memory.dat";
-    int totalActions = ReadFile(fileName);
-
-    // To test that it works....
-    cout << "Number of actions in memory.dat:\t " << totalActions;
+	string fileName = "memory.dat";
+	int totalActions = ReadFile(fileName);
+	int i = 0;
+	FIFO(totalActions);
+	// while(i < totalActions){
+	// 	cout << myPageTable[i].PROCESS_ID <<": ";
+	// 	cout << myPageTable[i].ACTION << "\t";
+	// 	cout << myPageTable[i].PAGE << "\n";
+	// 	i++;
+	// }
+	// To test that it works....
+	cout << "Number of actions in memory.dat:\t " << totalActions << "\n";
 }
 
 int ReadFile(string fileName)
@@ -65,14 +79,26 @@ int ReadFile(string fileName)
 	// Outside loop, we increment counter
 
 	while(!infile.eof()){
-		string temp; // string to hold current line input
-		while(getline(infile, temp)) // get the whole line, place in temp
-		{
-			// Parse string, place individual items in proper position
-			infile >> myProcess[i].PROCESS_ID >> myProcess[i].ACTION >> myProcess[i].PAGE;
-		}
-		// Exit while loop after a line, increment i
-		i++;
+    	bool check = true;
+    	while(check) // get the whole line, place in temp
+    	{
+        	// Parse string, place individual items in proper position
+        	infile >> myPageTable[i].PROCESS_ID >> myPageTable[i].ACTION;
+
+        	if((myPageTable[i].ACTION == "C")||(myPageTable[i].ACTION == "T"))
+        	{
+            	myPageTable[i].PAGE = 0;
+        	}
+
+        	else
+        	{
+            	infile >> myPageTable[i].PAGE;
+        	}
+        	cout << myPageTable[i].PROCESS_ID <<": "<< myPageTable[i].ACTION << ": "<< myPageTable[i].PAGE << endl;
+        	i++;
+        	check = false;
+    	}
+    	// Exit while loop after a line, increment i
 	}
 
 	// Close the file
@@ -82,9 +108,84 @@ int ReadFile(string fileName)
 	return i;
 }
 
-void FIFO(int)
-{
+void FIFO(int actions){
+	int i = 0;
+	typedef struct page_table{
+	int PID;
+	int PA;
+	int VA;
+	int dirty;
+	int AT;
+	}page_table;
+	page_table physical_page[20];
+	page_table swapping[80];
+	int active[100];
+	int time, j = 0;
+	while (i < (actions + 1)){
+    	//loop through myPageTable doing the actions
+    	if(myPageTable[i].ACTION == "C"){//create
+        	//add to active process array
+        	active[i] = myPageTable[i].PROCESS_ID;
+    	}
+    	if(myPageTable[i].ACTION == "T"){//terminate
+        	//process terminated
+        	//process cleaned from page table
+    	}
+    	if(myPageTable[i].ACTION == "A"){//allocate
+        	//allocate page for an active process
+        	//check for free pages to allocate
+        	for(j = 0; j < 20; j++){
+            	if(physical_page[j].PID == NULL){
+                	physical_page[j].PID = myPageTable[i].PROCESS_ID;
+                	physical_page[j].PA = j;
+                	physical_page[j].VA = myPageTable[i].PAGE;
+                	physical_page[j].dirty = 0;
+                	physical_page[j].AT = time;
+            	}
+        	}
+        	//swap FIFO to allocate
+        	for(j = 0; j < 20; j++){
+            	//find min arrival time (AT)
+            	//swap
+        	}
 
+    	}
+    	if(myPageTable[i].ACTION == "R"){//read
+        	//check page table
+        	for(j = 0; j < 20; j++){
+            	if(myPageTable[i].PROCESS_ID == physical_page[j].PID){
+                	if(myPageTable[i].PAGE == physical_page[j].VA){
+                    	physical_page[j].AT = time;
+                	}
+            	}
+        	}
+        	//check in swap
+        	for(j = 0 ; j< 20; j++){
+            	//find min arival time(AT)
+            	//swap
+        	}
+        	//accessed time increment
+    	}
+    	if(myPageTable[i].ACTION == "W"){//write
+        	//look at page table for allocated processes with pages
+        	for(j = 0; j < 20; j++){
+            	if(myPageTable[i].PROCESS_ID == physical_page[j].PID){
+                	if(myPageTable[i].PAGE == physical_page[j].VA){
+                    	physical_page[j].dirty = 1;
+                    	physical_page[j].AT = time;
+                	}
+            	}
+        	}
+        	//check in swap
+
+        	//mark dirty // accesed time increment
+    	}
+    	if(myPageTable[i].ACTION == "F"){//free
+        	//page freed
+    	}
+    	i++;
+    	time++;
+	}
 }
 
 void LRU(int)
